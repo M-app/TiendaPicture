6aa94b4ff178995209fbf8dcd29abffa05556e3c
Upvhas <xlopez3000@gmail.com>
2016-10-26 18:00:45 +0200

    Implementados métodos llenar jTable, comboBoxes
    He implementado algunos métodos para llenar el Jtable, pero
    sigo necesitando de productos y proveedores, y un procemiento
    almacenado más para seguir.

--- src/compras/ComprasController.java
+++ src/compras/ComprasController.java
@@ -1,42 +1,62 @@
 package compras;
 
 import java.util.ArrayList;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.table.DefaultTableModel;
+import productos.Productos;
+import productos.ProductosDTO;
 import proveedores.Proveedores;
+import proveedores.ProveedoresDTO;
 
 public class ComprasController {
     
-    private ComprasGUI comprasGui;
-    private ComprasDAO comprasDao;
+    private final ComprasGUI comprasGui;
+    private final ComprasDAO comprasDao;
     
     private Proveedores proveedores;
+    private Productos productos;
 
     public ComprasController(ComprasGUI gui) {
         this.comprasGui = gui;
         comprasDao = new ComprasDAO();
     }
     
-    public void llenarTabla(){
-        ArrayList<ComprasDTO> listaCompras = 
-                (ArrayList<ComprasDTO>) comprasDao.buscarCompras();
-        DefaultTableModel tableModel = (DefaultTableModel) comprasGui.tblCompras.getModel();
-        Object[] rowData = new Object[7];
-        for(ComprasDTO obj : listaCompras){
-            rowData[0] = obj.getIdCompra();
-            rowData[1] = obj.getMonto();
-            rowData[2] = obj.getFecha();
-            rowData[3] = obj.getNit();
-            rowData[4] = obj.getNoFactura();
-            rowData[5] = obj.getDireccion();
-            rowData[6] = proveedores.buscarProveedoresXCodigo(obj.getCodigoProveedor()).getNomContacto();
-            tableModel.addRow(rowData);
+    public void llenarTabla(int codigoProducto){
+        ProductosDTO producto = productos.buscarProductoXCodigo(codigoProducto);
+        Object[] columnas = new Object[8];
+        columnas[0] = producto.getCodigo();
+        columnas[1] = producto.getDescripcion();
+        columnas[2] = producto.getTamanio();
+        columnas[3] = producto.getColor();
+        columnas[4] = producto.getCapacidad();
+        columnas[5] = producto.getMarca();
+        columnas[6] = producto.getTipo();
+        columnas[7] = producto.getPrecioCosto();
+        columnas[8] = Float.parseFloat(comprasGui.txtSubTotal.getText());
+        DefaultTableModel modeloTabla = (DefaultTableModel) comprasGui.tblProductos.getModel();
+        modeloTabla.addRow(columnas);
+    }
+    
+    public String getSubtotal(int cantidad, float precioCosto){
+        return (cantidad * precioCosto) + "";
+    }
+   
+    public void llenarComboProductos(){
+        for(ProductosDTO producto : productos.buscarProductos()){
+            comprasGui.comboProductos.addItem(producto.getDescripcion());
+        }
+    }
+    
+    public void llenarComboProveedores(){
+        for(ProveedoresDTO proveedor : proveedores.buscarProveedores()){
+            comprasGui.comboProveedor.addItem(proveedor.getNomContacto());
         }
     }
     
     public void buscarCompraXCodigo(int codigo){
         ComprasDTO busqueda = comprasDao.buscarComprasXCodigo(codigo);
         if(busqueda != null){
-            DefaultTableModel tableModel = (DefaultTableModel) comprasGui.tblCompras.getModel();
+            DefaultTableModel tableModel = (DefaultTableModel) comprasGui.tblProductos.getModel();
             Object[] rowData = new Object[7];
             rowData[0] = busqueda.getIdCompra();
             rowData[1] = busqueda.getMonto();
