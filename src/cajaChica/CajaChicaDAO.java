00b32ac2c93b2450a2762af13c2b66823928015b
Oscar González <Oscar Gonzlez@KokinG7-PC>
2016-10-26 23:55:24 -0600

    Agregue el modelo de Productos, modelo de ventas, interfaz grafica ventas y controlador ventas.

--- src/cajaChica/CajaChicaDAO.java
+++ src/cajaChica/CajaChicaDAO.java
@@ -4,9 +4,29 @@ import domain.Conexion;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collection;
 
-public class CajaChicaDAO {
+public class CajaChicaDAO implements CajaChica{
+
     @Override
-    public CajaChicaDTO 
+    public CajaChicaDTO buscarCajaChicaXCodigo(int codigo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<CajaChicaDTO> buscarCajaChica() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void insertarCajaChica(float cantidad, String fecha, String tipo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminarCajaChica(int codigo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
     
 }
